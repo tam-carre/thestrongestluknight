9266d86b10ddec7d7346162c3af3f572ec969980
alnj <kokone@takat.su>
2021-06-17 20:59:56 +0200

    clean up/refactor

--- src/App.tsx
+++ src/App.tsx
@@ -3,32 +3,32 @@ import { Header } from 'components/Header'
 import { Credits } from 'components/Credits'
 import { Messages } from 'components/Messages'
 import { Navbar } from 'components/Navbar'
-import cocoLuna from './images/coco_with_luna_back_tatoo.png';
+import cocoLuna from 'images/coco_with_luna_back_tatoo.png';
+import kanaCoco from 'images/kanata_looking_at_dragon.png';
 import { AssetLoader, AssetLoaderProps, LoadStatus } from 'components/AssetLoader'
 
-const preloadAssets: AssetLoaderProps['assets'] = [{
-  href: cocoLuna,
-  as: 'image',
-}]
+const preloadAssets: AssetLoaderProps['assets'] = [
+  { href: cocoLuna, as: 'image' },
+  { href: kanaCoco, as: 'image' },
+]
 
-
-export function App() {
-  return <AssetLoader assets={preloadAssets}>
-    {LoadedApp}
-  </AssetLoader>
+export function App () {
+  return (
+    <AssetLoader assets={preloadAssets}>
+      {LoadedApp}
+    </AssetLoader>
+  )
 }
 
-export function LoadedApp (status: LoadStatus) {
-  // Block rendering until the image is loaded
-  if (status === LoadStatus.PENDING) {
-    return <div />
-  }
-  return (
-    <div className="App">
-      <Navbar />
-      <Header />
-      <Messages />
-      <Credits />
-    </div>
-  )
+///////////////////////////////////////////////////////////////////////////////
+
+function LoadedApp (status: LoadStatus) {
+  return status === LoadStatus.PENDING
+    ? <div>Loading!!!</div>
+    : <div className="App">
+        <Navbar />
+        <Header />
+        <Messages />
+        <Credits />
+      </div>
 }
--- src/components/AssetLoader.tsx
+++ src/components/AssetLoader.tsx
@@ -1,67 +1,53 @@
-import React from "react"
+import { useState, useEffect } from "react"
 
 export enum LoadStatus {
-    PENDING,
-    SUCCESS,
-    FAILED,
+  PENDING,
+  SUCCESS,
+  FAILED,
+}
+
+export function AssetLoader (props: AssetLoaderProps)  {
+  const [loads, setLoads] = useState <Record<href, boolean | undefined>> ({})
+  const [links, setLinks] = useState <JSX.Element[]> ([])
+
+  useEffect(() => {
+    const assetStatuses = Object.fromEntries (
+       props.assets.map (asset => [asset.href, undefined])
+    )
+
+    const links = props.assets.map (({href, as}, i) => <link
+      rel="preload"
+      href={href}
+      as={as}
+      key={i}
+      onLoad={ () => setLoads (prevState => ({ ...prevState, [href]: true,  }))}
+      onError={() => setLoads (prevState => ({ ...prevState, [href]: false, }))}
+    />)
+
+    setLoads(assetStatuses)
+    setLinks(links)
+
+  }, [props.assets])
+
+  const loadedStates = Object.values (loads ?? {})
+  const isPending    = loadedStates.some (loaded => loaded === undefined)
+  const isSuccess    = loadedStates.some (loaded => loaded !== false)
+
+  const loadStatus   = isPending ? LoadStatus.PENDING
+                     : isSuccess ? LoadStatus.SUCCESS
+                                 : LoadStatus.FAILED
+
+  return <> {links} {props.children (loadStatus)} </>
 }
 
 export type AssetLoaderProps = {
-    assets: Array<{
-        href: string,
-        as:string,
-        required?: boolean,
-    }>;
-    children: (status: LoadStatus) => React.ReactNode,
+  assets: Array<{
+    as:string,
+    href: string,
+  }>,
+  children: (status: LoadStatus) => React.ReactNode
 }
 
-export const AssetLoader = (props: AssetLoaderProps) => {
-    const [state, setState] = React.useState<Record<string, boolean | undefined> | null>(null);
-    const [links, setLinks] = React.useState<JSX.Element[]>([]);
-    React.useEffect(() => {
-        const links: JSX.Element[] = [];
-        const assets = props.assets.reduce((prev, curr) => { prev[curr.href] = undefined; return prev; }, {} as Record<string, boolean | undefined>);
-        setState(assets);
-        props.assets.forEach((asset, i) => {
-            links.push(<link 
-                rel="preload"
-                key={i}
-                href={asset.href} 
-                as={asset.as} 
-                onLoad={() => setState((prevState) => ({
-                    ...prevState,
-                    [asset.href]: true,
-                 }))}
-                onError={() => setState((prevState) => ({
-                    ...prevState,
-                    [asset.href]: true,
-                 }))}
-            />)
-        });
-        setLinks(links);
-    }, []);
+///////////////////////////////////////////////////////////////////////////////
 
-    if(!state) {
-        return <>
-            {props.children(LoadStatus.PENDING)}
-        </>
-    }
-
-    const stateValues = Object.values(state);
-    const pending = stateValues.some(v => v === undefined);
-    const success = stateValues.some(v => v !== false);
-
-    let loadState: LoadStatus = LoadStatus.PENDING;
-
-    if (!success) {
-        loadState = LoadStatus.FAILED;
-    } else if (!pending) {
-        loadState = LoadStatus.SUCCESS;
-    }
-
-    return <> 
-        {links}
-        {props.children(loadState)}
-    </>
-  
-}
+type href = string

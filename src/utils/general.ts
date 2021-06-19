ad5399ad6e2362fc651cee1693181bd2a11d20d7
alnj <kokone@takat.su>
2021-06-19 23:06:04 +0200

    Updates

--- src/utils/general.ts
+++
@@ -1,5 +0,0 @@
-export function doNothing () {}
-
-export function insertEveryN <T, U> (n: number, arr: T[], ins: U[]): (T|U)[] {
-  return arr.flatMap ((x, i) => i%n ? x : [x, ins[i/n]].filter (x => x))
-}

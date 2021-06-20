519604e9d159984bd30322b5a5043a89700d1b68
alnj <kokone@takat.su>
2021-06-20 16:04:02 +0200

    add some translations

--- src/data/text.ts
+++ src/data/text.ts
@@ -27,7 +27,7 @@ export const text: TranslationData = {
       jp: 'ルーナイトへ'
     },
   },
-  playlist: {
+  introduction: {
     firstParagraph: {
       en: 'Hey Kaichou, we Luknights want you to know that everyone is going to miss you dearly.',
       jp: '我々ルーナイトは、みんなが会長のことを心から寂しく思っていることを知ってほしいのです。',
@@ -35,6 +35,18 @@ export const text: TranslationData = {
     secondParagraph: {
       en: "Here's our HimeCoco playlist so everyone, including you, can remember the good times—and just a little further down, you can read everyone's parting words for you.",
       jp: "会長もみんなも楽しかった頃を思い出せるように、姫ココのプレイリストを紹介します。さらにその下には、みんなからのお別れの言葉が書かれています。",
+    }
+  },
+  playlist: {
+    himeCocoPlaylist: {
+      en: 'HimeCoco playlist',
+      jp: '姫ココプレーリスト'
+    }
+  },
+  messages: {
+    messagesFromLuknights: {
+      en: 'Messages from Luknights',
+      jp: 'ルーナイトからメッセージ'
     }
   },
   credits: {
